50ccf86ccc0811ae0f49f480e512eebe839e4592
shanmarais17@gmail.com <deXter17>
2017-08-03 22:54:51 +0200

    - Set token for map

--- app/src/main/java/inqb8/ansteph/oasis/testzone/Mapboxtest.java
+++ app/src/main/java/inqb8/ansteph/oasis/testzone/Mapboxtest.java
@@ -3,6 +3,7 @@ package inqb8.ansteph.oasis.testzone;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.constants.Style;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -19,14 +20,18 @@ public class Mapboxtest extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapboxtest);
 
+        Mapbox.getInstance(this, "pk.eyJ1IjoiY3Jhc2hvdnNraSIsImEiOiJjajVtaHNudmswcW9tMndyd21wYTk0bTBhIn0.h4u8fQtN7bvQuvmHvDNhKA");
+        setContentView(R.layout.activity_mapboxtest);
+
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        /*mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS);
             }
-        });
+        });*/
 
     }
 
