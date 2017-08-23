707c5b931905a4147838abafe08f6e06b0e3c924
shanmarais17@gmail.com <deXter17>
2017-08-23 10:51:53 +0200

    - Added Map marker for school map

--- app/src/main/java/inqb8/ansteph/oasis/mapping/SchoolMap.java
+++ app/src/main/java/inqb8/ansteph/oasis/mapping/SchoolMap.java
@@ -2,6 +2,7 @@ package inqb8.ansteph.oasis.mapping;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,8 +14,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.BounceInterpolator;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerViewOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
@@ -52,6 +59,9 @@ public class SchoolMap extends AppCompatActivity
         Mapbox.getInstance(this, "pk.eyJ1IjoiYW5zdGVwaCIsImEiOiJjajVoeG5qZ3QxbTY3MnhwbmN6ODE0bTA3In0.XZ6tlAzf1ynmBO7Lc_OK6A");
 
         // mapbox://styles/ansteph/cj65fp5dt6a5p2rpdy1r7o6zf
+        IconFactory iconFactory = IconFactory.getInstance(SchoolMap.this);
+        final Icon icon = iconFactory.fromResource(R.drawable.education_marker);
+        
 
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
@@ -59,6 +69,7 @@ public class SchoolMap extends AppCompatActivity
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 mapboxMap.setStyle("mapbox://styles/ansteph/cj67sdtdk0slv2smtp8xawfgm");
+                mapboxMap.addMarker(new MarkerViewOptions().position(new LatLng(-34.004441, 25.669534)).icon(icon));
             }
         });
 
@@ -163,4 +174,10 @@ public class SchoolMap extends AppCompatActivity
     public  void onViewListClicked (View v){
         startActivity(new Intent(getApplicationContext(), SchoolList.class));
     }
+
+    private void dropPinEffect(final Marker marker){
+
+    }
+
+
 }
